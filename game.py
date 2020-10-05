fa47a7524ab08100478edfe3b396766f054fd3ee
Builder212 <building2day@gmail.com>
2020-10-05 11:42:41 -0400

    Updated the game's logic and added a menu.

--- game.py
+++ game.py
@@ -3,65 +3,63 @@ import pygame, time, random
 class main_game:
 	def __init__(self, x, y, terrain_image, road_image, car_image, exploded_car, pothole_image):
 		self.screen = pygame.display.set_mode((x, y))
-		self.running = True
-		
+		self.menu = True
+		self.running = False
+		self.end = False
+
 		self.display_width = x
 		self.display_height = y
 		self.x = self.display_width*0.45
 		self.y = self.display_height*0.8+50
 		self.x_change = 0
-		self.speed = 2
+		self.speed = 1
+		self.distance = 0
 		self.y_val = 0
-		
+
+		self.death_screen = pygame.image.load("textures/death_screen.png")
+		self.menu_background = pygame.image.load("textures/menu_background.png")
 		self.terrain = pygame.image.load(terrain_image).convert_alpha()
 		self.road = pygame.image.load(road_image).convert_alpha()
-		
+
 		self.car = pygame.image.load(car_image).convert_alpha()
 		self.car_hitbox = (self.x, self.y, 51, 111)
-		self.exploded_car = pygame.image.load(exploded_car).convert_alpha()
-		
+
 		self.pothole = pygame.image.load(pothole_image).convert_alpha()
 		self.pothole_side = random.randrange(30, 100)
 		self.pothole_angle = random.randrange(0, 360)
 		self.pothole_startx = random.randrange(25, (self.display_width-25-self.pothole_side))
 		self.pothole_starty = -300
 		self.pothole_hitbox = ((self.pothole_side/2), self.pothole_startx, self.pothole_starty)
-		
-		self.explosion_1 = pygame.image.load("textures/explosion_1.png").convert_alpha()
-		self.explosion_2 = pygame.image.load("textures/explosion_2.png").convert_alpha()
-		self.explosion_3 = pygame.image.load("textures/explosion_3.png").convert_alpha()
-		self.explosion_4 = pygame.image.load("textures/explosion_4.png").convert_alpha()
-		self.explosion_5 = pygame.image.load("textures/explosion_5.png").convert_alpha()
-		self.explosion_6 = pygame.image.load("textures/explosion_6.png").convert_alpha()
-		self.explosion_7 = pygame.image.load("textures/explosion_7.png").convert_alpha()
-		self.exploded_car = pygame.image.load("textures/exploded_car.png").convert_alpha()
-		
+
+	def main_menu_setup(self):
+		self.screen.blit(self.menu_background, (0,0))
+		pygame.display.update()
 	def initial_load(self):
 		self.screen.blit(self.terrain, (0,0))
 		self.screen.blit(self.road, (25,0))
-	
+
 	def move_car(self, x, y):
 		self.screen.blit(self.car, (x, y))
-	
+
 	def events(self):
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				self.running = False
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_LEFT:
-					self.x_change = -2
+					self.x_change = -1
 				elif event.key == pygame.K_RIGHT:
-					self.x_change = 2
+					self.x_change = 1
 			if event.type == pygame.KEYUP:
 				if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
 					self.x_change = 0
-	
+
 	def car_onscreen(self):
 		if 0 < self.x + self.x_change < 300:
 			self.x += self.x_change
 		else:
 			pass
-	
+
 	def scrolling_road(self):
 		y = self.y_val % self.road.get_rect().height
 		self.screen.blit(self.terrain, (0, (y-self.terrain.get_rect().height)))
@@ -72,106 +70,29 @@ class main_game:
 		else:
 			pass
 		self.y_val += self.speed
-		
+
 	def potholes(self):
 		self.resized_pothole = pygame.transform.scale(self.pothole, (self.pothole_side, self.pothole_side))
 		self.rotated_pothole = pygame.transform.rotate(self.resized_pothole, self.pothole_angle)
 		self.screen.blit(self.resized_pothole, (self.pothole_startx, self.pothole_starty))
 		self.pothole_starty += self.speed
-		
+
 	def pothole_onscreen(self):
 		if self.pothole_starty > self.display_height:
 			self.pothole_starty = 0 - self.pothole_side
 			self.pothole_side = random.randrange(30, 100)
-			self.pothole_angle = random.randrange(0, 360)	
+			self.pothole_angle = random.randrange(0, 360)
 			self.pothole_startx = random.randrange(25, (self.display_width-25-self.pothole_side))
 		else:
 			pass
-	
+
 	def crash(self):
-		self.runnning = False
-		self.explosion_1 = pygame.transform.scale(self.explosion_1, (15, 15))
-		self.screen.blit(self.explosion_1, (self.x+18, self.y+5))
+		self.running = False
+		self.screen.blit(self.death_screen, (0,0))
 		pygame.display.update()
-		time.sleep(0.01)
-		self.explosion_1 = pygame.transform.scale(self.explosion_1, (17, 17))
-		self.screen.blit(self.explosion_1, (self.x+16, self.y+4))
-		pygame.display.update()
-		time.sleep(0.01)
-		self.explosion_1 = pygame.transform.scale(self.explosion_1, (20, 20))
-		self.screen.blit(self.explosion_1, (self.x+14, self.y+3))
-		pygame.display.update()
-		time.sleep(0.01)
-		self.screen.blit(self.explosion_2, (self.x+13, self.y+2))
-		pygame.display.update()
-		time.sleep(0.01)
-		self.explosion_2 = pygame.transform.scale(self.explosion_2, (27, 27))
-		self.screen.blit(self.explosion_2, (self.x+12, self.y))
-		pygame.display.update()
-		time.sleep(0.01)
-		self.explosion_2 = pygame.transform.scale(self.explosion_2, (30, 30))
-		self.screen.blit(self.explosion_2, (self.x+11, self.y-1))
-		pygame.display.update()
-		time.sleep(0.01)
-		self.explosion_2 = pygame.transform.scale(self.explosion_2, (33, 33))
-		self.screen.blit(self.explosion_2, (self.x+10, self.y-2))
-		pygame.display.update()
-		time.sleep(0.01)
-		self.explosion_2 = pygame.transform.scale(self.explosion_2, (36, 36))
-		self.screen.blit(self.explosion_2, (self.x+9, self.y-3))
-		pygame.display.update()
-		time.sleep(0.01)
-		self.explosion_3 = pygame.transform.scale(self.explosion_3, (40, 40))
-		self.screen.blit(self.explosion_3, (self.x+7, self.y-4))
-		pygame.display.update()
-		time.sleep(0.01)
-		self.explosion_3 = pygame.transform.scale(self.explosion_3, (44, 44))
-		self.screen.blit(self.explosion_3, (self.x+5, self.y-5))
-		pygame.display.update()
-		time.sleep(0.01)
-		self.explosion_3 = pygame.transform.scale(self.explosion_3, (48, 48))
-		self.screen.blit(self.explosion_3, (self.x+3, self.y-6))
-		pygame.display.update()
-		time.sleep(0.01)
-		self.explosion_3 = pygame.transform.scale(self.explosion_3, (52, 52))
-		self.screen.blit(self.explosion_3, (self.x+1, self.y-7))
-		pygame.display.update()
-		time.sleep(0.01)
-		self.explosion_3 = pygame.transform.scale(self.explosion_3, (56, 56))
-		self.screen.blit(self.explosion_3, (self.x-1, self.y-8))
-		pygame.display.update()
-		time.sleep(0.01)
-		self.explosion_4 = pygame.transform.scale(self.explosion_4, (70, 70))
-		self.screen.blit(self.explosion_4, (self.x-3, self.y-9))
-		pygame.display.update()
-		time.sleep(0.01)
-		self.explosion_4 = pygame.transform.scale(self.explosion_4, (80, 80))
-		self.screen.blit(self.explosion_4, (self.x-6, self.y-11))
-		pygame.display.update()
-		time.sleep(0.01)
-		self.explosion_4 = pygame.transform.scale(self.explosion_4, (100, 100))
-		self.screen.blit(self.explosion_4, (self.x-10, self.y-15))
-		pygame.display.update()
-		time.sleep(0.01)
-		self.explosion_4 = pygame.transform.scale(self.explosion_4, (150, 150))
-		self.screen.blit(self.explosion_4, (self.x-20, self.y-17))
-		pygame.display.update()
-		time.sleep(0.01)
-		self.screen.blit(self.terrain, (0,0))
-		self.screen.blit(self.road, (25,0))
-		self.exploded_car = pygame.transform.scale(self.exploded_car, (60, 120))
-		self.screen.blit(self.exploded_car, (self.x, self.y))
-		self.explosion_5 = pygame.transform.scale(self.explosion_5, (160, 160))
-		self.screen.blit(self.explosion_5, (self.x-35, self.y-20))
-		pygame.display.update()
-		time.sleep(0.01)
-		self.screen.blit(self.terrain, (0,0))
-		self.screen.blit(self.road, (25,0))
-		self.screen.blit(self.exploded_car, (self.x, self.y))
-		pygame.display.update()
-		time.sleep(10)
+		time.sleep(5)
 		exit()
-	
+
 	def is_collision(self):
 		self.car_hitbox = (self.x, self.y, 51, 111)
 		self.pothole_hitbox = ((self.pothole_side/2), self.pothole_startx, self.pothole_starty)
@@ -180,10 +101,29 @@ class main_game:
 				self.crash()
 		else:
 			pass
-	
+
+	def set_speed(self):
+		if self.distance < 20:
+			pass
+		elif self.distance >= 20:
+			self.speed = self.distance // 10
+			print(self.speed)
+
 	def mainloop(self):
+		self.main_menu_setup()
+		while self.menu:
+			for event in pygame.event.get():
+				if pygame.mouse.get_pressed()[0] == 1:
+					if 26 <= pygame.mouse.get_pos()[0] <= 300:
+						if 225 <= pygame.mouse.get_pos()[1] <= 310:
+							self.menu = False
+							self.running = True
+							time.sleep(0.2)
+			print(pygame.mouse.get_pos())
+
 		self.initial_load()
 		while self.running:
+			self.distance += 0.0005
 			self.events()
 			self.car_onscreen()
 			self.scrolling_road()
@@ -192,7 +132,8 @@ class main_game:
 			self.is_collision()
 			pygame.display.update()
 			self.pothole_onscreen()
-			
+			self.set_speed()
+
 if __name__ == "__main__":
 	pygame.init()
 	game = main_game(350, 814, "textures/grass_background.png", "textures/road_background.png", "textures/car.png", "textures/exploded_car.png", "textures/pothole.png")
